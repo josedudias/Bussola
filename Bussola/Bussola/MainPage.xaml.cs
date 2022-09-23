cff09b916212147b5004a411f1034f01b2b20158
jose.rufino01 <jose.rufino01@A10M07>
2022-09-23 14:42:53 -0300

    Adicionar arquivos de projeto.

---
+++ Bussola/Bussola/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bussola {
+    public partial class MainPage : ContentPage {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (!DesignMode.IsDesignModeEnabled)
+                ((BussolaViewModel)BindingContext).StartCommand.Execute(null);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (!DesignMode.IsDesignModeEnabled)
+                ((BussolaViewModel)BindingContext).StopCommand.Execute(null);
+        }
+    }
+}
---
+++ Bussola/Bussola/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Bussola {
+    public class BussolaViewModel : MvvmHelpers.BaseViewModel {
+        public BussolaViewModel()
+        {
+            StopCommand = new Command(Stop);
+            StartCommand = new Command(Start);
+        }
+        string headingDisplay;
+        public string HeadingDisplay
+        {
+            get => headingDisplay;
+            set => SetProperty(ref headingDisplay, value);
+        }
+
+        double heading = 0;
+        public double Heading
+        {
+            get => heading;
+            set => SetProperty(ref heading, value);
+        }
+
+        public Command StopCommand { get; }
+
+        void Stop()
+        {
+            if (!Compass.IsMonitoring)
+                return;
+
+            Compass.ReadingChanged -= Compass_ReadingChanged;
+            Compass.Stop();
+        }
+
+
+        public Command StartCommand { get; }
+
+        void Start()
+        {
+            if (Compass.IsMonitoring)
+                return;
+
+
+            Compass.ReadingChanged += Compass_ReadingChanged;
+            Compass.Start(SensorSpeed.UI, true);
+
+        }
+
+        void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            Heading = e.Reading.HeadingMagneticNorth;
+            HeadingDisplay = $"Heading: {Heading.ToString()}";
+        }
+    }
+}
